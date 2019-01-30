0a967e6bb79b54ac1660348907aa64ae3df46015
Monilito Castro <mannycastro.epic@gmail.com>
2019-01-29 21:26:19 -0600

    first

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <AppHeader></AppHeader>
+</template>
+<script>
+import AppHeader from "./components/AppHeader";
+
+export default {
+    name: "App",
+    components: {
+        AppHeader
+    }
+}
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="ui secondary pointing menu">
+            <a class="active item">
+                Image Storage
+            </a>
+            <div class="right menu">
+                <a class="ui item">
+                Gallery
+                </a>
+                <a class="ui item">
+                Upload
+                </a>
+                <a class="ui item">
+                Logout
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppHeader"
+    }
+</script>
+
+<style>
+
+</style>
